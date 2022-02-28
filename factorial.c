d66e7c88bb1bbcd749a0298edc202c1155910e7b
Ali Sharify <87280202+alisharifyy@users.noreply.github.com>
2022-02-28 15:43:09 +0330

    add actuall file

---
+++ factorial.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//github : alisharifyy :)
+
+//function prototype
+void factorial(long number,int fac);
+
+int main(void)
+{
+
+    long number = 0;
+    printf("\n[Github] : alisharifyy\n");
+    printf("[HELLO]\nThis is factorial\n\n");
+
+    //Get input From user
+    printf("Enter Your Number:");
+    scanf("%li",&number);
+
+    int fac = number;
+    factorial(number,fac);
+}
+
+void factorial(long number,int fac)
+{
+    //safety check For infinity loop
+    if (fac <= 1)
+    {
+        printf("Factorial is : %li \n",number);
+        return ;
+    }
+    fac -= 1;
+    number = number * fac;
+    factorial(number,fac);
+}
+
